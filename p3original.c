275a6801026fadce0a69228b8bbda69beac12ad7
sanjanaraghupat <96220467+sanjanaraghupathi@users.noreply.github.com>
2021-12-29 04:41:07 +0000

    add any two numbers using 4 functions and pass by reference

---
+++ p3original.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void input(int *a,int *b)
+{
+  printf("first number:\n");
+  scanf("%d",a);
+  printf("second number:\n");
+  scanf("%d",b);
+}
+void add(int a, int b, int *sum)
+{
+  *sum =a+b;
+}
+void output(int a, int b, int sum){
+  printf("the sum of %d and %d is %d\n",a,b,sum);
+
+}
+int main(){
+  int a,b,sum;
+  input(&a,&b);
+  add(a,b,&sum);
+  output(a,b,sum);
+  return 0;
+
+}
