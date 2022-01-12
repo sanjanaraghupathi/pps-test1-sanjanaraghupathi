8b8633b19f6a0696726d5ca07a0df5b868124c74
sanjanaraghupat <96220467+sanjanaraghupathi@users.noreply.github.com>
2022-01-12 04:08:41 +0000

    sum using 4 functions

--- p2final.c
+++ p2final.c
@@ -1,23 +1,26 @@
 #include<stdio.h>
 int input()
+
 {
   int a;
-  printf("enter a number");
+  printf("Enter a number");
   scanf("%d",&a);
   return a;
-
 }
+
 int add(int a, int b)
 {
   int sum;
   sum = a+b;
   return sum;
 }
+
 void output(int a, int b, int sum)
 {
   printf("the sum of %d and %d is %d",a, b, sum);
 
 }
+
 int main()
 {
   int x,y,z;
