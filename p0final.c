ae5a4deb6136dfbec255a2ec74d77eccdedbf624
sanjanaraghupat <96220467+sanjanaraghupathi@users.noreply.github.com>
2022-01-12 03:43:50 +0000

    my name is sanjana

--- p0final.c
+++ p0final.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main ()
 {
-   printf("sanjana");
+   printf("my name is sanjana\n");
 return 0;
  }
--- p3original.c
+++ p3original.c
@@ -10,7 +10,8 @@ void add(int a, int b, int *sum)
 {
   *sum =a+b;
 }
-void output(int a, int b, int sum){
+void output(int a, int b, int sum)
+{
   printf("the sum of %d and %d is %d\n",a,b,sum);
 
 }
